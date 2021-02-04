ac10f747a2c8dfc4f67d987724ad4509047ff1c5
Mihir <mwadekar2000@gmail.com>
2021-02-03 18:45:31 -0800

    Added Basic Server code

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/user/<user_id>/form/<form_id>', methods = ['GET', 'POST'])
+def form(user_id, form_id):
+    print(request.json)
+    return "xyz"
+
+if __name__ == "__main__":
+    app.run(port = 8080)
